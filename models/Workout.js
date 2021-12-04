3ce4ea82f75b335795007671728297d9fb7ad520
Brian D. Baker <brian.devs.baker@gmail.com>
2021-12-03 21:43:52 -0500

    Developed models

---
+++ models/Workout.js
@@ -0,0 +1,62 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const exerciseSchema = new Schema({
+  type: {
+    type: String,
+    trim: true,
+    required: "Specify a type of exercise: 'cardio' or 'resistance'",
+    enum: ['cardio', 'resistance']
+  },
+  name: {
+    type: String,
+    trim: true,
+    required: "Specify a name for the exercise (e.g. 'benchpress', 'elliptical')"
+  },
+  duration: {
+    type: Number,
+    min: 0,
+    required: "Specify a duration for the exercise"
+  },
+  distance: {
+    type: Number,
+    min: 0,
+    required: function() {
+      return (this.type === 'cardio' ? 'You must specify a distance for cardio workouts' : false);
+    }
+  },
+  weight: {
+    type: Number,
+    min: 0,
+    required: function() {
+      return (this.type === 'resistance' ? 'You must specify a weight for resistance workouts' : false);
+    }
+  },
+  sets: {
+    type: Number,
+    min: 0,
+    required: function() {
+      return (this.type === 'resistance' ? 'You must specify the number of sets for resistance workouts' : false);
+    }
+  },
+  reps: {
+    type: Number,
+    min: 0,
+    required: function() {
+      return (this.type === 'resistance' ? 'You must specify the number of reps for resistance workouts' : false);
+    }
+  }
+});
+
+const workoutSchema = new Schema({
+  day: {
+    type: Date,
+    required: "Specify a date for the workout"
+  },
+  exercises: [exerciseSchema]
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
